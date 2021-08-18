c4011716433d658313800da6b8cc6d0de310936a
abayomi <abayominice@gmail.com>
2021-08-18 17:35:53 +0100

    Essential changes

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.send('This is the new landing page')
 })
 
+//Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+//Set views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+  res.render('Home')
+})
+
+app.get('', (req, res) => {
+  res.render('About')
+})
 
 app.listen(port, () => {
 	  console.log(`Example app listening at http://localhost:${port}`)
