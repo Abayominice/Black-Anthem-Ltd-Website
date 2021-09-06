3795870358d6a4c51d3f01975c133717a1e5a0ae
abayomi <abayominice@gmail.com>
2021-09-06 17:22:22 +0100

    final commits

--- app.js
+++ app.js
@@ -1,68 +1,4 @@
-// const express = require('express')
-// const path = require('path')
-
-// console.log("@about")
-
-// const app = express()
-// const port = 3000
-
-
-
-// //Static Files
-// app.use(express.static(path.resolve(__dirname, 'public')))
-
-
-// var usersRouter = require('./public/js/users');
-// app.use('/api', usersRouter);
-
-
-// app.get('/form', function(req, res, next) { 
-//   res.render('Home'); 
-//   console.log(req);
-//   })
-
-//   var db=require('../../database');
-
-// router.post('/create', function(req, res, next) {
-//   console.log(req)
-//   // store all the user input data
-//   const userDetails=req.body;
- 
-//   // insert user data into users table
-//   var sql = 'INSERT INTO users SET ?';
-//   db.query(sql, userDetails,function (err, data) { 
-//       if (err) throw err;
-//          console.log("User dat is inserted successfully "); 
-//   });
-//  res.redirect('/Home');  // redirect to user form page after inserting the data
-// });
-
-// //Set views
-// app.set('view engine', 'ejs')
-
-// app.set('views', './views')
-
-// app.get('/', (req, res) => {
-//   console.log("@home")
-//   res.render('Home')
-// })
-
-// app.get('/About', (req, res) => {
-//   console.log("@about")
-//   res.render('About')
-// })
-
-// app.get('/Projects', (req, res) => {
-//   res.render('Projects')
-// })
-
-// app.get('/RAQ', (req, res) => {
-//   res.render('RAQ')
-// })
-// app.listen(port, () => {
-// 	  console.log(`Example app listening at http://localhost:${port}`)
-// })
-
+require('dotenv').config({});
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -78,12 +14,23 @@ app.use(express.urlencoded({
   extended:true,
 }))
 
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USERNAME,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+});
+ 
+connection.connect(function(err){
+  if (err) throw err;
+  console.log('connected..')
+});
+
 
 //Set views
 app.set('view engine', 'ejs')
 
 app.set('views', './views')
-
 
 
 app.get('/About', (req, res) => {
@@ -99,38 +46,16 @@ app.get('/RAQ', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/home.ejs'));
+  res.render('home');
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-})
-
-// app.get('/raq', (req, res) => {
-//   res.render('raq', {qs: req.query});
-// })
-// app.post('/raq', urlencodedParser, (req, res) => {
-//   res.render('raq', {qs: req.query});
-// })
-var connection = mysql.createConnection({
-  host     : 'https://www.blackanthemltd.site',
-  user     : 'root',
-  password : 'Mynameisabayomi1.',
-  database : 'bal'
-});
- 
-connection.connect(function(err){
-  if (err) throw err;
-  console.log('connected..')
-});
-
-app.post('/', function(req,res){
-  console.log(req.body);
-
-  var sql ="insert into users values('"+req.body.fn+"','"+req.body.ln+"',"+req.body.email+")"
+app.post('/', function(req, res) {
+  var sql =`insert into bal values('${req.body.fname}', '${req.body.lname}', '${req.body.email}')`;
   connection.query(sql, function (error, results) {
     if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+    app.get('/503page', (req, res) => {
+      res.render('503page');
+    })
   });
 
   res.render('Home', { title: 'Data Saved',
@@ -138,33 +63,6 @@ app.post('/', function(req,res){
 
   connection.end();
 })
-
-// var connection2 = mysql.createConnection({
-//   host     : 'https://www.blackanthemltd.site',
-//   user     : 'abayomi',
-//   password : '',
-//   database : 'Quotation_requests'
-// });
- 
-// connection2.connect(function(err){
-//   if (err) throw err;
-//   console.log('connected..')
-// });
-
-// app.post('/RAQ', function(req,res){
-//   console.log(req.body);
-
-//   var sql ="insert into users values('"+req.body.sfname+"', '"+req.body.slname+"', '"+req.body.semail+"', '"+req.body.services+"', "+req.body.comment+")"
-//   connection2.query(sql, function (error, results) {
-//     if (error) throw error;
-//     console.log('The solution is: ', results[0].solution);
-//   });
-
-//   res.render('RAQ', { title: 'Data Saved',
-//   message: 'Data Saved successfully.'})
-
-//   connection2.end();
-// })
 
 app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`)
